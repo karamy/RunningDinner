3f6a000d3a3fbab22361852ed23b2523889adb26
Andrea Sfargeri <andreasfargeri@gmail.com>
2020-02-27 15:15:44 +0100

    Added Camera in profile-photo

--- src/app/sign-up/address-map/address-map.page.ts
+++ src/app/sign-up/address-map/address-map.page.ts
@@ -3,12 +3,14 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
+
 @Component({
   selector: 'app-address-map',
   templateUrl: './address-map.page.html',
   styleUrls: ['./address-map.page.scss'],
 })
 export class AddressMapPage implements OnInit {
+  
 
   constructor(private authService: AuthService, private router: Router) { }
 
--- src/app/sign-up/profile-photo/profile-photo.page.ts
+++ src/app/sign-up/profile-photo/profile-photo.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { PhotoService } from './photo.service';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
 
 @Component({
   selector: 'app-profile-photo',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePhotoPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {
+    defineCustomElements(window);
   }
 
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo, position) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+         }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
