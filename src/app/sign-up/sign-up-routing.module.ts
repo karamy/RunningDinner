b0abc758185262e8d683b6abcfeed499c8a5ca56
carlo <carlo.caramia@gmail.com>
2020-03-27 23:42:51 +0100

    ulteriore sistemazione rotte: - rimosso l'uso di SignupPage in quanto inutile perchè si va direttamente a username

--- src/app/sign-up/sign-up-routing.module.ts
+++ src/app/sign-up/sign-up-routing.module.ts
@@ -1,27 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SignUpPage } from './sign-up.page';
-
 const routes: Routes = [
+  { path: '', redirectTo: 'username', pathMatch: 'full' },
   {
-    path: '',
-    component: SignUpPage
-  },  {
     path: 'username',
-    loadChildren: () => import('./username/username.module').then( m => m.UsernamePageModule)
+    loadChildren: () => import('./username/username.module').then(m => m.UsernamePageModule)
   },
   {
     path: 'birth-date',
-    loadChildren: () => import('./birth-date/birth-date.module').then( m => m.BirthDatePageModule)
+    loadChildren: () => import('./birth-date/birth-date.module').then(m => m.BirthDatePageModule)
   },
   {
     path: 'profile-photo',
-    loadChildren: () => import('./profile-photo/profile-photo.module').then( m => m.ProfilePhotoPageModule)
+    loadChildren: () => import('./profile-photo/profile-photo.module').then(m => m.ProfilePhotoPageModule)
   },
   {
     path: 'address-map',
-    loadChildren: () => import('./address-map/address-map.module').then( m => m.AddressMapPageModule)
+    loadChildren: () => import('./address-map/address-map.module').then(m => m.AddressMapPageModule)
   }
 
 ];
@@ -30,4 +26,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class SignUpPageRoutingModule {}
+export class SignUpPageRoutingModule { }
