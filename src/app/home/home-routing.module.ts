e1bdf120d12683d7a942be1180e49753d33609dc
carlo <carlo.caramia@gmail.com>
2020-04-10 01:02:58 +0200

    minor, virgola di troppo

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -31,6 +31,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class HomePageRoutingModule { }
