04d361a50bdeeeeea40d4dd9e9e8448357ea9e79
carlo <carlo.caramia@gmail.com>
2020-04-20 23:12:47 +0200

    Auto-generated commit

--- capacitor-pwa-firebase-msg-sw.js
+++ capacitor-pwa-firebase-msg-sw.js
@@ -1,5 +1,19 @@
 
 (function() {
+    // Ho dovuto impostare la funzione che gestisce il click sulla notifica prima di importare le librerie client di Firebase perchè ne annullano il comportamento
+    self.addEventListener('notificationclick', (event) => {
+        console.log('[Service Worker] Notification click Received. event', event);
+        event.notification.close();
+        
+        if (clients.openWindow && event.notification.data.url) {
+            event.waitUntil(clients.openWindow(event.notification.data.url));
+        } else {
+            if(clients.openWindow){
+                event.waitUntil(clients.openWindow('https://localhost:8080'));
+            }
+        }
+    });
+
   importScripts('./firebase-app.js');
   importScripts('./firebase-messaging.js');
 
