382bfdd8053c8d2763836844efa36be1f64b4aa8
carlo <carlo.caramia@gmail.com>
2020-05-17 19:15:09 +0200

    Chat, fixato comportamento per cui all'invio di un messaggio non scrollava in fondo, e alla prima ricezione dei messaggi si posizioni alla fine

--- src/app/home/tabs/chat/chat.page.ts
+++ src/app/home/tabs/chat/chat.page.ts
@@ -10,7 +10,7 @@ import { IonContent } from '@ionic/angular';
 })
 export class ChatPage implements OnInit {
   // Recupero attraverso annotation @ViewChild il riferimento all'elemento del DOM su cui effettuare lo scroll
-  @ViewChild('content', { static: false }) content: IonContent;
+  @ViewChild('chatContent', { static: false }) chatContent: IonContent;
   newMsgText: String;
   dinnermessages: ChatMsg[] = [];
   user: UserData;
@@ -21,20 +21,26 @@ export class ChatPage implements OnInit {
     // Inizializzazione chat e caricamento messaggi
     this.user = this.authService.getUserData();
     this.chatService.getDinnerMessagesObservable().subscribe((firebaseMessages) => {
-      this.dinnermessages = this.dinnermessages = firebaseMessages;
+      if (this.dinnermessages.length == 0) { // Scrollo in fondo solo alla prima ricezione dei messaggi
+        this.scrollToBottom();
+      }
+      this.dinnermessages = firebaseMessages;
     });
   }
 
   // Invia il messaggio, portando la videata a scrollare in fondo
   sendMsg() {
     this.chatService.addNewMessage(this.newMsgText).then(() => {
-      this.content.scrollToBottom();
+      this.scrollToBottom();
       this.newMsgText = '';
     });
   }
-  // scrollto() {
-  //   setTimeout(() => {
-  //       this.content.scrollToBottom();
-  //     }, 1000);
-  //   }
+
+  // Effettua lo scroll del contenuto della chat, inserendolo all'interno di
+  // un timeout per permettere ad angular di effettuare un ciclo di digest e caricare i dati
+  scrollToBottom() {
+    setTimeout(() => {
+      this.chatContent.scrollToBottom();
+    });
+  }
 }
